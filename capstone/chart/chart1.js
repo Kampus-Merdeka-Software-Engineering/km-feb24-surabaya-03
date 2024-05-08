450357fcacdb9ff5beb5afa60dd663e08a4c3cc9
NIKO RIANT CAHYONO <nikoriantcahyono@gmail.com>
2024-05-08 23:57:58 +0700

    ga update apa apa gan

--- capstone/chart/chart1.js
+++ capstone/chart/chart1.js
@@ -19,3 +19,32 @@ const chart1 = document.getElementById('total_sales_by_month');
         }
     });
    
+const chart2 = document.getElementById('top_five_product_sales');
+
+new Chart(chart2, {
+    type: 'bar',
+    data: {
+        labels: ['Poland Springs Water', 'Cheezlt', 'Sunchip A', 'Sunchip B', 'Sunchip C'],
+        datasets: [{
+            label: 'Total Sales',
+            data: [500, 300, 200, 150, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132, 0.2)',
+    
+            ],
+            borderColor: [
+                'rgb(255, 99, 132, 1)',
+    
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
