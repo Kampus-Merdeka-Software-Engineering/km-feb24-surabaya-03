6dcec5fe419a3244dc4adac2aabec5d0219a8f61
nikoriantcahyono <nikoriantcahyono@gmail.com>
2024-06-06 18:06:19 +0000

    update css

--- script.js
+++ script.js
@@ -310,6 +310,27 @@ function getDataLineTotal(dataGet){
 
     revenueValue.innerHTML = `$ ${revenue}`;
 }
+function showModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+window.onload = function() {
+    showModal();
+}
 
 function visualization() {
     // Total Sales by Month - Grafik Line
